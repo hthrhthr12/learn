e973858065d6050f88b2771d679b257eea07719c
yair <yairgalili1@gmail.com>
2020-04-23 15:35:20 +0300

    matlab intermediate exercises up to 3

--- matlab/hospital.m
+++ matlab/hospital.m
@@ -23,3 +23,28 @@ plot_normal_distribution_fit(blood_pressure_non_smokers,'non smokers',[.2 .5 .3 
 legend('location','best')
 title('normal distribution fit of the left column of the blood pressure')
 xlabel('left column of the blood pressure')
+
+%%
+% Count all the last names who contains ES
+clc
+close all
+disp(['num of last names who contains ES: ',num2str(sum(contains(hospital.LastName,'ES')))]);
+% Plot histogram of last names lengths
+histogram(cellfun(@length,hospital.LastName))
+xlabel('last name length')
+title('histogram of last name lengths')
+grid on
+%%
+%Count all the last names that ends with S
+clc
+close all
+ends_with_s=num2str(sum(endsWith(hospital.LastName,'S')));
+disp(['num of last names who ends with S: ',ends_with_s]);
+%%
+%Replace the first letter by the next alphabetic letter for each last name.
+clc
+close all
+
+cellfun(@(word){char(word+[1,zeros(1,length(word)-1)])},hospital.LastName)
+
+
