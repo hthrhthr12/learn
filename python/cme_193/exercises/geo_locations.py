cbdf18ecbd54d9bca26151211b524408f34f6add
yair <yairgalili1@gmail.com>
2020-05-04 17:02:44 +0300

    fixed plots

--- python/cme_193/exercises/geo_locations.py
+++ python/cme_193/exercises/geo_locations.py
@@ -21,7 +21,6 @@ import pandas as pd
 from geopy import distance
 from shapely.geometry import MultiPoint, Point
 
-data = pd.read_csv('geonames-database\geonames.csv', usecols=['latitude', 'longitude', 'country code'])
 EARTH_RADIUS_KM = distance.EARTH_RADIUS
 WGS84_DEGREE = "EPSG:4326"  # units: degree
 # EPSG:3395 uses the elliptical version of the Marcator projection
@@ -31,72 +30,88 @@ WGS84_METER_ELLIPSE = "EPSG:3395"  # units: m, World Mercator
 # WGS 84 / Pseudo-Mercator -- Spherical Mercator
 WGS84_METER_SPHERE = 3857
 
+data = pd.read_csv('geonames-database\geonames.csv', usecols=['latitude', 'longitude', 'country code'])
+
 
 def plot_map(mean_latitude=31.0461, mean_longitude=34.8516,
-             radius_meters=7 * 10 ** 3, print_by_matplotlib=True, is_open_map=False):
-    """plot on a map places in a specific radius
+             radius_meters=7 * 10 ** 3, print_by_matplotlib=True, is_open_map=False, only_filter=False):
+    """
+    plot on a map places in a specific radius
     assuming the longitude is not close to zeros lines
     """
     # (latitude, longitude)
     radius_km = radius_meters / 10 ** 3
-    ratio_distance_earth = radius_km / EARTH_RADIUS_KM
-    lat_bounds = mean_latitude - ratio_distance_earth, mean_latitude + ratio_distance_earth
-    # I add absolute value to the equation in:
-    # https://www.movable-type.co.uk/scripts/latlong-db.html
-    lon_width = abs(np.arcsin(ratio_distance_earth) / np.cos(mean_latitude))
-    lon_bounds = mean_longitude - lon_width, mean_longitude + lon_width
-    plot_lat_lon_limits(mean_longitude, mean_latitude, lon_bounds, lat_bounds,
-                        radius_meters, print_by_matplotlib, is_open_map)
+    mean_location = mean_latitude, mean_longitude
+    circle_search = gpd.GeoSeries(Point(mean_location[-1::-1]), crs=WGS84_DEGREE)
+    circle_search_meters = circle_search.to_crs(crs=WGS84_METER_SPHERE).buffer(radius_meters)
+    circle_search = circle_search_meters.to_crs(crs=WGS84_DEGREE)
+    # circle_search.bounds.values:
+    # min_longitude, min_latitude, max_longitude, max_latitude
+    min_max_lon_lat = circle_search.bounds.values[0]
+    geo_data_filtered = data[data.latitude.between(min_max_lon_lat[1], min_max_lon_lat[3])]
+
+    if np.sign(min_max_lon_lat[0]) == np.sign(min_max_lon_lat[2]):
+        # do not pass zero
+        geo_data_filtered = geo_data_filtered[
+            geo_data_filtered.longitude.between(min_max_lon_lat[0], min_max_lon_lat[2])]
+    # reset index
+    geo_data_filtered.reset_index(drop=True, inplace=True)
+    # geodesic distance
+    geo_data_filtered = geo_data_filtered[geo_data_filtered.apply(
+        lambda location: distance.distance((location.latitude, location.longitude), mean_location) <= radius_km,
+        axis=1)]
+
+    # locations = gpd.GeoSeries(gpd.points_from_xy(geo_data_filtered['longitude'],geo_data_filtered['latitude']))
+    geo_data_meter = gpd.GeoDataFrame(geo_data_filtered['country code'], crs=WGS84_DEGREE,
+                                      geometry=gpd.points_from_xy(geo_data_filtered['longitude'],
+                                                                  geo_data_filtered['latitude'])).to_crs(
+        crs=WGS84_METER_SPHERE)
+
+    # geo_data = geo_data[geo_data.geometry.within(circle_search)]
+    # geo_data_meter = geo_data_meter[geo_data_meter.geometry.within(circle_search_meters)]
+    # can be solved by distance.distance
+    if only_filter:
+        return mean_location, circle_search_meters, geo_data_meter
+    plot_locations(mean_location, circle_search_meters, geo_data_meter, print_by_matplotlib, is_open_map)
+
+    # data_filtered.apply(
+    #     lambda location: distance.distance((location.latitude, location.longitude), mean_location),
+    #     axis=1)
+    #
+    # geo_data = gpd.GeoDataFrame(geo_data_filtered['country code'], crs=WGS84_DEGREE,
+    #                             geometry=gpd.points_from_xy(geo_data_filtered['longitude'],
+    #                                                         geo_data_filtered['latitude']))
 
 
-def plot_map_azimuth_aperture(mean_longitude=1.5, mean_latitude=42.5,
-                              azimuth=10, aperture=10, radius_meters=7 * 10 ** 3, print_by_matplotlib=True,
+def plot_map_azimuth_aperture(mean_latitude=31.0461, mean_longitude=34.8516,
+                              azimuth=10, aperture=30000, radius_meters=7 * 10 ** 3, print_by_matplotlib=True,
                               is_open_map=False):
     """plot on a map places in a specific radius
     assuming the longitude is not close to zeros lines
     """
     # (latitude, longitude)
-    radius_km = radius_meters / 10 ** 3
-    ratio_distance_earth = radius_km / EARTH_RADIUS_KM
-    lat_bounds = mean_latitude - ratio_distance_earth, mean_latitude + ratio_distance_earth
+    mean_location, circle_search, geo_data_meter = plot_map(mean_latitude, mean_longitude,
+                                                            radius_meters, only_filter=True)
+    center = circle_search.centroid
+    xc, yc = float(center.x), float(center.y)
 
-    lon_width = abs(np.arcsin(ratio_distance_earth) / np.cos(mean_latitude))
-    lon_bounds = max(mean_longitude - lon_width, azimuth - aperture), \
-                 min(mean_longitude + lon_width, azimuth + aperture)
+    # azimuth<45 degrees
+    tan_over_1 = 1 / np.tan(azimuth * np.pi / 180)
+    d_times_sqrt = aperture * np.sqrt(tan_over_1 ** 2 + 1)
+    m1 = d_times_sqrt + xc * tan_over_1 + yc
+    m2 = -d_times_sqrt + xc * tan_over_1 + yc
+    shift_max, shift_min = max(m1, m2), min(m1, m2)
+    x_val = np.array([point.x for point in geo_data_meter.geometry])
+    y_val = np.array([point.y for point in geo_data_meter.geometry])
+    data_line = y_val + tan_over_1 * x_val
+    geo_data_meter = geo_data_meter[(data_line >= shift_min) & (data_line <= shift_max)]
+    plot_locations(mean_location, circle_search, geo_data_meter, print_by_matplotlib, is_open_map)
 
-    plot_lat_lon_limits(mean_longitude, mean_latitude, lon_bounds, lat_bounds,
-                        radius_meters, print_by_matplotlib, is_open_map)
 
-
-def plot_lat_lon_limits(mean_longitude=31.0461, mean_latitude=34.8516, lon_bounds=(-180, 180), lat_bounds=(-90, 90),
-                        radius_meters=7 * 10 ** 3, print_by_matplotlib=True, is_open_map=False):
-    """plot on a map places in a specific radius
-    """
-    radius_km = radius_meters / 10 ** 3
-
-    # we assume that the region of search can pass one of limits -180,180
-    # but not both
-    # If the region passed, correct search:
-    is_in_latitude_region = data.latitude.between(lat_bounds[0], lat_bounds[1])
-    if lon_bounds[1] > 180:
-        is_in_longitude_region = (lon_bounds[0] < data.longitude) | (data.longitude < lon_bounds[1] - 360)
-    elif lon_bounds[0] < -180:
-        is_in_longitude_region = (lon_bounds[0] + 360 < data.longitude) | (data.longitude < lon_bounds[1])
-    else:
-        is_in_longitude_region = data.longitude.between(lon_bounds[0], lon_bounds[1])
-
-    data_filtered = data[is_in_latitude_region & is_in_longitude_region]
-
-    mean_location = mean_latitude, mean_longitude
-
-    # if there is some data, obeys the latitude, longitude bounds
-    if not data_filtered.empty:
-        is_in_region = data_filtered.apply(
-            lambda location: distance.distance((location.latitude, location.longitude), mean_location) <= radius_km,
-            axis=1)
-        data_filtered = data_filtered[is_in_region]
-
-    if data_filtered.empty:
+def plot_locations(mean_location, circle_search, geo_data, print_by_matplotlib, is_open_map):
+    """plot locations"""
+    mean_latitude, mean_longitude = mean_location
+    if geo_data.empty:
         # return closest longitude and latitude:
         closest_lon = max(data.longitude[data.longitude < mean_longitude]), min(
             data.longitude[data.longitude > mean_longitude])
@@ -106,22 +121,19 @@ def plot_lat_lon_limits(mean_longitude=31.0461, mean_latitude=34.8516, lon_bound
               f"closest latitude to {mean_latitude}: minimal:{closest_lat[0]},maximal:{closest_lat[1]}")
         return
 
-    geo_data = gpd.GeoDataFrame(data_filtered['country code'],
-                                crs=WGS84_DEGREE,
-                                geometry=gpd.points_from_xy(data_filtered['longitude'], data_filtered['latitude']))
     convex_hull = MultiPoint([point for point in geo_data['geometry']]).convex_hull
-    polygon = gpd.GeoDataFrame(index=[0], crs=WGS84_DEGREE, geometry=[convex_hull])
-    countries = data_filtered["country code"].unique()
+    polygon = gpd.GeoDataFrame(index=[0], crs=WGS84_METER_SPHERE, geometry=[convex_hull])
+    countries = geo_data["country code"].unique()
     print(countries)
-    polygon_area = polygon.to_crs(WGS84_METER_ELLIPSE).area[0]
+    polygon_area = polygon.area[0]
     print(f"area of polygon in m^2: {polygon_area}")
 
     if print_by_matplotlib:
-        circle_search = gpd.GeoSeries(Point(mean_location[-1::-1]), crs=WGS84_DEGREE)
-
-        circle_search = circle_search.to_crs(crs=WGS84_METER_SPHERE).buffer(radius_meters)
-        geo_data = geo_data.to_crs(crs=WGS84_METER_SPHERE)
-        polygon = polygon.to_crs(crs=WGS84_METER_SPHERE)
+        # circle_search = gpd.GeoSeries(Point(mean_location[-1::-1]), crs=WGS84_DEGREE)
+        #
+        # circle_search = circle_search.to_crs(crs=WGS84_METER_SPHERE).buffer(radius_meters)
+        # geo_data = geo_data.to_crs(crs=WGS84_METER_SPHERE)
+        # polygon = polygon.to_crs(crs=WGS84_METER_SPHERE)
 
         ax = geo_data.plot(marker='*', color='red', markersize=12)
         polygon.plot(ax=ax, facecolor='none', edgecolor='k')
@@ -141,25 +153,19 @@ def plot_lat_lon_limits(mean_longitude=31.0461, mean_latitude=34.8516, lon_bound
             os.system('map.html')
 
 
-plot_map(radius_meters=7 * 10 ** 5, print_by_matplotlib=True)
-plot_map(radius_meters=7 * 10 ** 5, print_by_matplotlib=False)
 plot_map_azimuth_aperture()
-plot_map_azimuth_aperture(azimuth=10, aperture=10, radius_meters=7 * 10 ** 3, print_by_matplotlib=True)
 
-plot_map_azimuth_aperture(azimuth=-10, aperture=1, radius_meters=7 * 10 ** 5, print_by_matplotlib=True)
-
-plot_map_azimuth_aperture(azimuth=10, aperture=10, radius_meters=7 * 10 ** 5, print_by_matplotlib=True)
-plot_map(print_by_matplotlib=True)
-
+plot_map(radius_meters=7 * 10 ** 4, print_by_matplotlib=True)
+plot_map(radius_meters=7 * 10 ** 5, print_by_matplotlib=False)
+plot_map_azimuth_aperture(mean_latitude=31.0461, mean_longitude=34.8516,
+                          azimuth=10, aperture=30000, radius_meters=7 * 10 ** 3, print_by_matplotlib=True)
 # israel:
-# 31.0461° N, 34.8516°E
+# lat: 31.0461° N, lon: 34.8516°E
 # 36 UTM
 
 
 # country on the board:
-plot_map(mean_longitude=35.7860, mean_latitude=33.3058, radius_meters=7 * 10 ** 5)
-
-
+plot_map(mean_longitude=35.7860, mean_latitude=33.3058, radius_meters=7 * 10 ** 3)
 
 # circle_search.buffer(11)
 # create polygon
